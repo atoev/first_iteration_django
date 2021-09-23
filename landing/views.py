80a108714b902f3f1826d96d32c5d7ecb5842cad
Odilbek Arziev <odilbekarziev1802@mail.ru>
2021-09-23 23:32:35 +0500

    4 lesson

--- landing/views.py
+++ landing/views.py
@@ -5,4 +5,10 @@ from .models import Course, Teacher
 
 def home(request):
     courses = Course.objects.all()
-    return render(request, 'home.html', {'courses': courses})
+    teachers = Teacher.objects.all()
+    return render(request, 'home.html', {'courses': courses, 'teachers': teachers})
+
+
+def about(request, pk):
+    course = Course.objects.get(pk=pk)
+    return render(request, 'about.html', {'course': course})
